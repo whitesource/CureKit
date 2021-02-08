c513689038212813f355ad5286513b18dd1d9b01
Shani <shani.kagan@whitesourcesoftware.com>
2021-02-08 16:47:46 +0200

    encodeForLog

--- src/main/java/com/wss/remediation/RemUtils.java
+++ src/main/java/com/wss/remediation/RemUtils.java
@@ -27,13 +27,15 @@ public class RemUtils {
   public static String[] encodeForLog(Object... content) {
           String[] result = new String[content.length];
           for (int i = 0; i < content.length; i++) {
-            result[i] = content[i].toString()
-                    .replaceAll("[\n|\r|\t]", "_")
-                    .replaceAll("<", "&lt")
-                    .replaceAll(">", "&gt");
+            result[i] = RemUtils.encodeForLog(content[i]);
           }
           return result;
   }
 
-
+  public static String encodeForLog(Object content){
+    return content.toString()
+            .replaceAll("[\n|\r|\t]", "_")
+            .replaceAll("<", "&lt")
+            .replaceAll(">", "&gt");
+  }
 }
