2d798f33aff1472b6ca2632d468b85d99e49b97b
shlomieyaniv <shlomie.yaniv@whitesourcesoftware.com>
2021-03-17 14:14:22 +0200

    Added Test class

---
+++ src/test/java/com/wss/remediation/RemUtilsTest.java
@@ -0,0 +1,19 @@
+package com.wss.remediation;
+
+import org.junit.Test;
+//import org.junit.junit.RunWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+//import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+class RemUtilsTest {
+
+
+    @Test
+    public void encodeForOS() {
+
+        assert true;
+
+    }
+}
