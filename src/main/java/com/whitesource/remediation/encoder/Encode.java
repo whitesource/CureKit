855950ad1faf6c2d5ae351a8dcc526736814f6af
shlomieyaniv <shlomie.yaniv@whitesourcesoftware.com>
2021-04-04 15:59:33 +0300

    Improved documentations

--- src/main/java/com/whitesource/remediation/encoder/Encode.java
+++ src/main/java/com/whitesource/remediation/encoder/Encode.java
@@ -1,12 +1,11 @@
 package com.whitesource.remediation.encoder;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.NonNull;
 import org.apache.commons.lang3.SystemUtils;
 import org.owasp.esapi.codecs.UnixCodec;
 import org.owasp.esapi.codecs.WindowsCodec;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Remediation Solver static class written by WhiteSource with the community ❤. Here you can find
@@ -55,29 +54,29 @@ public class Encode {
    */
   public static String logContentEncoder(@NonNull final Object content) {
     return content
-            .toString()
-            .replaceAll("[\n|\r|\t]", "_")
-            .replaceAll("<", "&lt")
-            .replaceAll(">", "&gt");
+        .toString()
+        .replaceAll("[\n|\r|\t]", "_")
+        .replaceAll("<", "&lt")
+        .replaceAll(">", "&gt");
   }
 
   /**
-   * Encoding content for logs.
+   * This method encodes for JavaScript strings contained within HTML script blocks.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded JavaScript block.
    */
   public static String forJavaScriptBlock(@NonNull final String content) {
 
     return org.owasp.encoder.Encode.forJavaScriptBlock(content);
   }
 
-
   /**
-   * Encoding content for logs.
+   * This method encodes for HTML text content. It does not escape quotation characters and is thus
+   * unsafe for use with HTML attributes. Use either forHtml or forHtmlAttribute for those methods.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded Html content.
    */
   public static String forHtmlContent(@NonNull final String content) {
 
@@ -85,10 +84,10 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * This method encodes for HTML text attributes.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded Html Attribute.
    */
   public static String forHtmlAttribute(@NonNull final String content) {
 
@@ -96,10 +95,17 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * Encodes for a JavaScript string. It is safe for use in HTML script attributes (such as
+   * onclick), script blocks, JSON files, and JavaScript source. The caller MUST provide the
+   * surrounding quotation characters for the string. Since this performs additional encoding so it
+   * can work in all of the JavaScript contexts listed, it may be slightly less efficient than using
+   * one of the methods targetted to a specific JavaScript context (forJavaScriptAttribute(String),
+   * forJavaScriptBlock(java.lang.String), forJavaScriptSource(java.lang.String)). Unless you are
+   * interested in saving a few bytes of output or are writing a framework on top of this library,
+   * it is recommend that you use this method over the others.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded JavaScript string.
    */
   public static String forJavaScript(@NonNull final String content) {
 
@@ -107,10 +113,11 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * Encodes for CSS strings. The context must be surrounded by quotation characters. It is safe for
+   * use in both style blocks and attributes in HTML.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded CSS String.
    */
   public static String forCssString(@NonNull final String content) {
 
@@ -118,10 +125,12 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * Performs percent-encoding for a component of a URI, such as a query parameter name or value,
+   * path or query-string. In particular this method insures that special characters in the
+   * component do not get interpreted as part of another component.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded Uri component.
    */
   public static String forUriComponent(@NonNull final String content) {
 
@@ -129,10 +138,13 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * Encodes for CSS URL contexts. The context must be surrounded by "url(" and ")". It is safe for
+   * use in both style blocks and attributes in HTML. Note: this does not do any checking on the
+   * quality or safety of the URL itself. The caller should insure that the URL is safe for
+   * embedding (e.g. input validation) by other means.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded CSS url.
    */
   public static String forCssUrl(@NonNull final String content) {
 
@@ -140,10 +152,17 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * Encodes for unquoted HTML attribute values. forHtml(String) or forHtmlAttribute(String) should
+   * usually be preferred over this method as quoted attributes are XHTML compliant. When using this
+   * method, the caller is not required to provide quotes around the attribute (since it is encoded
+   * for such context). The caller should make sure that the attribute value does not abut unsafe
+   * characters--and thus should usually err on the side of including a space character after the
+   * value. Use of this method is discouraged as quoted attributes are generally more compatible and
+   * safer. Also note, that no attempt has been made to optimize this encoding, though it is still
+   * probably faster than other encoding libraries.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded Html unquoted Attribute.
    */
   public static String forHtmlUnquotedAttribute(@NonNull final String content) {
 
@@ -151,14 +170,16 @@ public class Encode {
   }
 
   /**
-   * Encoding content for logs.
+   * This method encodes for JavaScript strings contained within HTML script attributes (such as
+   * onclick). It is NOT safe for use in script blocks. The caller MUST provide the surrounding
+   * quotation characters. This method performs the same encode as forJavaScript(String) with the
+   * exception that / is not escaped.
    *
    * @param content {@link Object} contains the content.
-   * @return encoded log content.
+   * @return encoded JavaScript attribute.
    */
   public static String forJavaScriptAttribute(@NonNull final String content) {
 
     return org.owasp.encoder.Encode.forJavaScriptAttribute(content);
   }
-
 }
