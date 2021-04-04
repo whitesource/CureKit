6736f3e445d32726ea630957a093664fe991609c
shlomieyaniv <shlomie.yaniv@whitesourcesoftware.com>
2021-04-04 12:18:15 +0300

    Added Xss Encoders

--- src/main/java/com/whitesource/remediation/encoder/Encode.java
+++ src/main/java/com/whitesource/remediation/encoder/Encode.java
@@ -1,11 +1,12 @@
 package com.whitesource.remediation.encoder;
 
-import java.util.ArrayList;
-import java.util.List;
 import lombok.NonNull;
 import org.apache.commons.lang3.SystemUtils;
 import org.owasp.esapi.codecs.UnixCodec;
 import org.owasp.esapi.codecs.WindowsCodec;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Remediation Solver static class written by WhiteSource with the community ❤. Here you can find
@@ -54,9 +55,110 @@ public class Encode {
    */
   public static String logContentEncoder(@NonNull final Object content) {
     return content
-        .toString()
-        .replaceAll("[\n|\r|\t]", "_")
-        .replaceAll("<", "&lt")
-        .replaceAll(">", "&gt");
+            .toString()
+            .replaceAll("[\n|\r|\t]", "_")
+            .replaceAll("<", "&lt")
+            .replaceAll(">", "&gt");
   }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forJavaScriptBlock(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forJavaScriptBlock(content);
+  }
+
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forHtmlContent(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forHtmlContent(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forHtmlAttribute(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forHtmlAttribute(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forJavaScript(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forJavaScript(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forCssString(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forCssString(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forUriComponent(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forUriComponent(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forCssUrl(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forCssUrl(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forHtmlUnquotedAttribute(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forHtmlUnquotedAttribute(content);
+  }
+
+  /**
+   * Encoding content for logs.
+   *
+   * @param content {@link Object} contains the content.
+   * @return encoded log content.
+   */
+  public static String forJavaScriptAttribute(@NonNull final String content) {
+
+    return org.owasp.encoder.Encode.forJavaScriptAttribute(content);
+  }
+
 }
